0c5a5e44f7bbe23d442711c0b24a0a1ec74b0a81
Smit Rao <smitrao@outlook.com>
2018-07-10 22:35:20 -0400

    Added new favicon and Deadline models

---
+++ TimeTrackWeb/Models/Deadline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackWeb.Models
+{
+    public class Deadline
+    {
+        public DateTime Time { get; set; }
+        public String Id = new Guid().ToString();
+        public String Name { get; set; }
+    }
+}
---
+++ TimeTrackWeb/Models/Deadlines.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackWeb.Models
+{
+    public class Deadlines
+    {
+        public Deadline[] DeadlineData { get; set; } // this is an array of Deadline objects
+
+    }
+}
